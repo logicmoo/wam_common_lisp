e784ca43c9ea568a51df909ae11e29db5aedbc04
root <logicmoo@gmail.com>
2017-11-03 05:35:26 -0700

    doc string and symbol info

--- prolog/wam_cl/groveler.lisp
+++ prolog/wam_cl/groveler.lisp
@@ -1,5 +1,5 @@
-#+sbcl
-(defun psyminfo ()
+
+(defun docinfo ()
  (dolist (p (list-all-packages))
   (let (( pn (string-downcase (package-name p))))
    (format t "package_nickname('~a','~a').~%" pn pn)
@@ -9,23 +9,18 @@
      (format t "package_use_list('~a','~a').~%" pn (string-downcase (package-name nn))))
    (dolist (nn (package-shadowing-symbols p))
      (format t "package_shadowing_symbols('~a','~a').~%" pn nn))
- (when (and (not (string= "sb-c" pn))(not (string= "sb-vm" pn)))
+
  (do-all-symbols (sym)    
   (when (eq (symbol-package sym) p)   
     (let (( sn (string-downcase (symbol-name sym))))
-     
-     (format t "symbol_info('~a','~a',package,~(~a~)).~%" sn pn (second (multiple-value-list  (find-symbol (symbol-name sym) p))))
-     (when (boundp sym) 
-       (handler-case  
-        (progn (format t "symbol_info('~a','~a',value,'~a',~a).~%" sn pn (symbol-value sym) (if (constantp sym) "constant" "variable"))
-      (let ((doc (documentation sym 'variable))) (when doc (format t "doc_string('~a','~a',variable,\"~a\").~%" sn pn (princ-to-string doc)))))))
-     (when (fboundp sym) 
-      (let* ((sf (symbol-function sym)) (sft (class-name (class-of sf))))
-       (format t "symbol_info('~a','~a',function_type,'~(~a~)').~%" sn pn sft)
-       (let ((doc (documentation sym 'function))) (when doc (format t "doc_string('~a','~a',function,\"~a\").~%" sn pn (princ-to-string doc))))))
-     '(when (symbol-plist sym) (format t "symbol_plist('~a','~a').~%" sn  (symbol-plist sym)))
+       (let ((doc (documentation sym 'function))) (when doc (format t "doc_string('~a','~a',function,~a).~%" sn pn (write-to-string doc))))
+       (let ((doc (documentation sym 'variable))) (when doc (format t "doc_string('~a','~a',variable,~a).~%" sn pn (write-to-string doc))))
+     (when (special-operator-p sym)
+         (format t "special('~a','~a').~%" sn pn))
 
-)))))))
+))))))
+
+(docinfo)
 
 
 #-sbcl
