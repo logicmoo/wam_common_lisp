9f3b0fe2daa5eb6de0e229686841878e00af3983
root <logicmoo@gmail.com>
2017-11-11 20:11:30 -0800

    fixed constantp

--- prolog/wam_cl/prims.pl
+++ prolog/wam_cl/prims.pl
@@ -33,11 +33,13 @@ prims:cl_exact.
 
 :- use_module(library('dialect/sicstus/arrays')).
 % :- use_module(library('dialect/sicstus')).
-is_self_evaluationing_object(X):- var(X),!.
+
+% Numbers, pathnames, and arrays are examples of self-evaluating objects.
 is_self_evaluationing_object(X):- atomic(X),!,is_self_evaluationing_const(X).
+is_self_evaluationing_object(X):- var(X),!.
 is_self_evaluationing_object(X):- (is_dict(X);is_array(X);is_rbtree(X)),!.
 
-is_self_evaluationing_const(X):- atomic(X),!,(number(X);is_keywordp(X);string(X);(blob(X,T),T\==text);X=t;X=[]),!.
+is_self_evaluationing_const(X):- atomic(X),!,(X==t;X==[];number(X);is_keywordp(X);string(X);(blob(X,T),T\==text)),!.
 
 
 user:op_replacement(first,cl_car).
