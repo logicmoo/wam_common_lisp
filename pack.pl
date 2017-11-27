ccedd8ce5af61c08975256ba40135d650c180bcd
root <logicmoo@gmail.com>
2017-11-27 09:37:43 -0800

    added missing deps

--- pack.pl
+++ pack.pl
@@ -7,11 +7,14 @@ packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
 maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
 home( 'https://github.com/TeamSPoon/wam_common_lisp' ).
 download( 'https://github.com/TeamSPoon/wam_common_lisp/release/*.zip' ).
-requires(loop_check).
-requires(predicate_streams).
-requires(no_repeats).
 requires(dictoo).
-requires(must_trace).
 requires(logicmoo_utils).
+requires(loop_check).
+requires(must_trace).
+requires(no_repeats).
+requires(predicate_streams).
+requires(with_open_options).
+requires(with_thread_local).
+requires(xlisting).
 autoload(false).
 
